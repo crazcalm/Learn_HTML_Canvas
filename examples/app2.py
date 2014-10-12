ccccea541d6579beacb4a511b1a34bfb32477546
marcuswillock <marcus@adcade.com>
2014-10-12 00:31:43 -0400

    The ogv video is working

--- examples/app2.py
+++ examples/app2.py
@@ -6,6 +6,10 @@ app = flask.Flask(__name__)
 def hello_world():
     return flask.render_template("index.html")
 
+@app.route("/video")
+def video_world():
+    return flask.render_template("video.html")
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
