a2f6feed879ba6ace1d0c69ddc6a8fbcdd45cdef
marcuswillock <marcus@adcade.com>
2014-10-15 20:57:11 -0400

    I gave the canvas game its own route

--- examples/app2.py
+++ examples/app2.py
@@ -10,6 +10,10 @@ def hello_world():
 def video_world():
     return flask.render_template("video.html")
 
+@app.route("/videoGame")
+def video_game():
+    return flask.render_template("videoGame.html")
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
